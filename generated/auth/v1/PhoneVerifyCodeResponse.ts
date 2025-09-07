1a9f84b0fd999cf0264f54104cf478d3c3d97909
TeaCoder52 <vnechaev888@gmail.com>
2025-09-07 14:40:32 +0300

    chore: index.js

--- generated/auth/v1/PhoneVerifyCodeResponse.ts
+++ generated/auth/v1/PhoneVerifyCodeResponse.ts
@@ -4,11 +4,9 @@
 export interface PhoneVerifyCodeResponse {
   'ok'?: (boolean);
   'accessToken'?: (string);
-  'refreshToken'?: (string);
 }
 
 export interface PhoneVerifyCodeResponse__Output {
   'ok'?: (boolean);
   'accessToken'?: (string);
-  'refreshToken'?: (string);
 }
